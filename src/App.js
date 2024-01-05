bad9202bb9681d449e9ebe097ab641b6a72a9cc5
AjayManinder <102263680+AjayManinder@users.noreply.github.com>
2024-01-04 22:44:16 -0600

    Revert "Feature/task2"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
+import logo from './logo.svg';
 import './App.css';
-import {  Route, Routes } from 'react-router-dom';
-import Home from './pages/home';
-import Table from './pages/Table';
-import Header from './Header/header';
-// import Footer from './footer/footer';
+
 function App() {
   return (
-   <>
-     <Header/>
-     <Routes>
-      
-        <Route exact path="/" element=<Home /> />
-       <Route path="/table" element=<Table /> />
-        
-
- 
-    </Routes>
-    {/* <Footer/> */}
-    </>
-   
+    <div className="App">
+     <>
+     </>
+    </div>
   );
 }
 
