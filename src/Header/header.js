bf35ad7ba2d65b5340bee65a9e9b7c33f9bcc891
AjayManinder <102263680+AjayManinder@users.noreply.github.com>
2024-02-01 13:17:22 -0600

    header and login changes

--- src/Header/header.js
+++ src/Header/header.js
@@ -1,18 +1,45 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import "./header.css";
 import { Link, Navigate } from "react-router-dom";
+import { jwtDecode } from 'jwt-decode';
+import axiosInstance from "../axiosConfig";
+import { IoLogOut } from "react-icons/io5";
 
 const Header = ({ authenticated, setAuthenticated }) => {
+  const [userDetails, setUserDetails] = useState(null);
 
-  const [showDropdown, setShowDropdown] = useState(false);
-
-  const handleToggleDropdown = () => {
-    setShowDropdown(!showDropdown);
-  };
+  useEffect(() => {
+    const fetchUserDetails = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        if (token && authenticated) {  // Add a check for authenticated
+          const decodedToken = jwtDecode(token);
+          const userId = decodedToken?.user_id;
+  
+          if (userId) {
+            const response = await axiosInstance.get(`/users/${userId}`);
+            setUserDetails(response.data);
+          } else {
+            console.error('User ID not found in the decoded token. Decoded token:', decodedToken);
+            setUserDetails(null);
+          }
+        } else {
+          setUserDetails(null);
+        }
+      } catch (error) {
+        console.error('Error fetching user details:', error);
+        setUserDetails(null);
+      }
+    };
+  
+    fetchUserDetails();
+  }, [authenticated]);
+  
 
   const handleLogout = () => {
     localStorage.removeItem('token');
     setAuthenticated(false);
+    setUserDetails(null);
   };
 
   return (
@@ -32,27 +59,35 @@ const Header = ({ authenticated, setAuthenticated }) => {
           <Link className="Header_Links" to="/">
             HOME
           </Link>
-          {authenticated && (
+
+          {authenticated && userDetails ? (
             <>
               <Link className="Header_Links" to="/table">
                 TABLE
               </Link>
-              <button className="Header_Links" onClick={handleLogout}>
-                LOGOUT
-              </button>
+              <div className="Header-Userdetails">
+              
+              <div className="Header_Links_User">
+               <strong>{userDetails.email || 'User'} </strong> 
+              </div>
+              <div className="Header_Links_Button" onClick={handleLogout}>            
+              <strong><IoLogOut /></strong>
+              </div>
+              </div>
+              
             </>
-          )}
-          {!authenticated && (
+          ) : (
             <>
-            <Link className="Header_Links" to="/login">
-              LOGIN
-            </Link>
+              <Link className="Header_Links" to="/login">
+               Login
+              </Link>
+              {/* Add the TABLE link here if needed */}
             </>
           )}
         </div>
       </div>
       {/* Conditional rendering of Navigate component */}
-      {authenticated && <Navigate to="/table" />}
+      {/* {authenticated && <Navigate to="/table" />} */}
     </div>
   );
 };
--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,20 +1,29 @@
-// axiosConfig.js
 import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL;
 const React_Host = process.env.REACT_APP_React_Host;
 const React_Port = process.env.REACT_APP_React_Port;
 
-// Get the token from local storage
-const token = localStorage.getItem('token');
-
 // Create an Axios instance with default configuration
 const axiosInstance = axios.create({
   baseURL: `${API_URL}://${React_Host}:${React_Port}`,
   headers: {
     'Content-Type': 'application/json',
-    'Authorization': `Bearer ${token}`, // Set the Authorization header
   },
 });
+
+// Add a request interceptor to include the authorization token in each request
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
